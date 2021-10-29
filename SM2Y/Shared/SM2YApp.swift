f9aa733e1d5e75fbdbf1b631e9d18108d24f6c6d
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2021-10-28 23:57:57 -0300

    Commit 1
    Add Inicial

---
+++ SM2Y/Shared/SM2YApp.swift
@@ -0,0 +1,17 @@
+//
+//  SM2YApp.swift
+//  Shared
+//
+//  Created by Bruno Cassiano on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SM2YApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
