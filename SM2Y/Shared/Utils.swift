7e7370fd32924016e050e34da8f94deb90f1e40b
Bruno Cassiano <48299655+bcassiano@users.noreply.github.com>
2021-10-29 08:07:12 -0300

    Incial

---
+++ SM2Y/Shared/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  SM2Y (iOS)
+//
+//  Created by Bruno Cassiano on 29/10/21.
+//
+
+import Foundation
+
+class Utils {
+    
+    static let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
